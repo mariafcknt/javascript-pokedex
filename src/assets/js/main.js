8a4d8bc464415bd613d6efe86fadf8fba447274a
Maria <mariafcknt@outlook.com>
2023-12-21 13:31:21 -0300

    fix colors

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,20 +1,14 @@
-
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
 function convertPokemonToLi(pokemon) {
     return `
-    <li class = "pokemon">
-    <span class="number">#001</span> 
+    <li class = "pokemon ${pokemon.type}">
+    <span class="number">#${pokemon.number}</span> 
     <span class="name">${pokemon.name}</span>
 
     <div class="detail">
         <ol class="types">
-            <li class="type">grass</li>
-            <li class="type">poison</li>
+            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
         </ol>
-        <img src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+        <img src = "${pokemon.photo}" alt="${pokemon.name}">
     </div>
     </li>        
     `
@@ -23,9 +17,5 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons = []) => {
-
-    const newList = pokemons.map((pokemon) => convertPokemonToLi(pokemon))
-
-    const newHtml = newList.join('')
-    pokemonList.innerHTML += newHtml
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
 })

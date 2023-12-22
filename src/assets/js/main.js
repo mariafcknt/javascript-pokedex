8024fc994d2c4503371ddc17699aeab54e3a3fe2
Maria <mariafcknt@outlook.com>
2023-12-22 12:59:39 -0300

    create pagination limit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,8 @@
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton');
-const limit = 5;
+const limit = 10;
 let offset = 0;
+const maxRecords = 151;
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
@@ -26,5 +27,13 @@ loadPokemonItens(offset, limit);
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit;
-    loadPokemonItens(offset, limit);
+    const qtdRecordWithNextPage = offset + limit;
+
+    if (qtdRecordWithNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset;
+        loadPokemonItens(offset, newLimit);
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
+    } else {
+        loadPokemonItens(offset, limit);
+    }
 })

952ab430204586a932ba42858e537538885eb3b8
Maria <mariafcknt@outlook.com>
2023-12-22 13:02:40 -0300

    fix limit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,6 @@
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton');
-const limit = 10;
+const limit = 5;
 let offset = 0;
 const maxRecords = 151;
 
